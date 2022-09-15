b40b3ddfb24fa5af6733fe1f850caa9f1a6a491c
Hoang Nguyen <nvthoang@gmail.com>
2022-09-16 02:41:27 +1200

    utilities

---
+++ utils.py
@@ -0,0 +1,14 @@
+import torch 
+
+
+#custom weights initialization called on generator and discriminator
+def init_weights(net, init_type='normal', scaling=0.02):
+    def init_func(m):  # define the initialization function
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv')) != -1:
+            torch.nn.init.normal_(m.weight.data, 0.0, scaling)
+        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+            torch.nn.init.normal_(m.weight.data, 1.0, scaling)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    print('initialize network with %s' % init_type)
+    net.apply(init_func)  # apply the initialization function <init_func>
